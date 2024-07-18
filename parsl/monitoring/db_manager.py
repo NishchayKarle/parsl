NishchayKarle/parsl

commit 666250e8c711ba76eacc1a22f9adafc4425dd439

Add much more aggressive type checking for monitoring (#1936)
One of the main points of this is to work towards more explicit descriptions
of what goes over each queue.

Another is that this code has been poorly tested in CI in the past, and
typechecking provides more resilience there.

Co-authored-by: Zhuozhao Li <zhuozhao@uchicago.edu>

parsl/monitoring/db_manager.py
@@ -5,7 +5,7 @@ import os
 import time
 import datetime
 
-from typing import Any, Dict, List, Set
+from typing import Any, Dict, List, Optional, Set, Tuple, TypeVar
 
 from parsl.log_utils import set_file_logger
 from parsl.dataflow.states import States
@@ -14,9 +14,11 @@ from parsl.monitoring.message_type import MessageType
 
 logger = logging.getLogger("database_manager")
 
+X = TypeVar('X')
+
 try:
     import sqlalchemy as sa
-    from sqlalchemy import Column, Text, Float, Boolean, Integer, DateTime, PrimaryKeyConstraint
+    from sqlalchemy import Column, Text, Float, Boolean, Integer, DateTime, PrimaryKeyConstraint, Table
     from sqlalchemy.orm import sessionmaker
     from sqlalchemy.ext.declarative import declarative_base
 except ImportError:
@@ -54,7 +56,7 @@ class Database:
     Base = declarative_base()
 
     def __init__(self,
-                 url='sqlite:///monitoring.db',
+                 url: str = 'sqlite:///monitoring.db',
                  ):
 
         self.eng = sa.create_engine(url)
@@ -66,25 +68,25 @@ class Database:
         Session = sessionmaker(bind=self.eng)
         self.session = Session()
 
-    def update(self, table=None, columns=None, messages=None):
-        table = self.meta.tables[table]
-        mappings = self._generate_mappings(table, columns=columns,
+    def update(self, *, table: str, columns: List[str], messages: List[Dict[str, Any]]) -> None:
+        table_obj = self.meta.tables[table]
+        mappings = self._generate_mappings(table_obj, columns=columns,
                                            messages=messages)
-        mapper = get_mapper(table)
+        mapper = get_mapper(table_obj)
         self.session.bulk_update_mappings(mapper, mappings)
         self.session.commit()
 
-    def insert(self, table=None, messages=None):
-        table = self.meta.tables[table]
-        mappings = self._generate_mappings(table, messages=messages)
-        mapper = get_mapper(table)
+    def insert(self, *, table: str, messages: List[Dict[str, Any]]) -> None:
+        table_obj = self.meta.tables[table]
+        mappings = self._generate_mappings(table_obj, messages=messages)
+        mapper = get_mapper(table_obj)
         self.session.bulk_insert_mappings(mapper, mappings)
         self.session.commit()
 
-    def rollback(self):
+    def rollback(self) -> None:
         self.session.rollback()
 
-    def _generate_mappings(self, table, columns=None, messages=[]):
+    def _generate_mappings(self, table: Table, columns: Optional[List[str]] = None, messages: List[Dict[str, Any]] = []) -> List[Dict[str, Any]]:
         mappings = []
         for msg in messages:
             m = {}
@@ -239,15 +241,15 @@ class Database:
 
 class DatabaseManager:
     def __init__(self,
-                 db_url='sqlite:///monitoring.db',
-                 logdir='.',
-                 logging_level=logging.INFO,
-                 batching_interval=1,
-                 batching_threshold=99999,
+                 db_url: str = 'sqlite:///monitoring.db',
+                 logdir: str = '.',
+                 logging_level: int = logging.INFO,
+                 batching_interval: float = 1,
+                 batching_threshold: float = 99999,
                  ):
 
         self.workflow_end = False
-        self.workflow_start_message = None
+        self.workflow_start_message = None  # type: Optional[Dict[str, Any]]
         self.logdir = logdir
         os.makedirs(self.logdir, exist_ok=True)
 
@@ -261,12 +263,16 @@ class DatabaseManager:
         self.batching_interval = batching_interval
         self.batching_threshold = batching_threshold
 
-        self.pending_priority_queue = queue.Queue()  # type: queue.Queue[Any]
-        self.pending_node_queue = queue.Queue()  # type: queue.Queue[Any]
-        self.pending_block_queue = queue.Queue()  # type: queue.Queue[Any]
-        self.pending_resource_queue = queue.Queue()  # type: queue.Queue[Any]
+        self.pending_priority_queue = queue.Queue()  # type: queue.Queue[Tuple[MessageType, Dict[str, Any]]]
+        self.pending_node_queue = queue.Queue()  # type: queue.Queue[Dict[str, Any]]
+        self.pending_block_queue = queue.Queue()  # type: queue.Queue[Dict[str, Any]]
+        self.pending_resource_queue = queue.Queue()  # type: queue.Queue[Dict[str, Any]]
 
-    def start(self, priority_queue, node_queue, block_queue, resource_queue) -> None:
+    def start(self,
+              priority_queue: "queue.Queue[Tuple[MessageType, Dict[str, Any]]]",
+              node_queue: "queue.Queue[Dict[str, Any]]",
+              block_queue: "queue.Queue[Dict[str, Any]]",
+              resource_queue: "queue.Queue[Dict[str, Any]]") -> None:
 
         self._kill_event = threading.Event()
         self._priority_queue_pull_thread = threading.Thread(target=self._migrate_logs_to_internal,
@@ -494,7 +500,7 @@ class DatabaseManager:
                                       'hostname'],
                              messages=reprocessable_first_resource_messages)
 
-    def _migrate_logs_to_internal(self, logs_queue, queue_tag, kill_event):
+    def _migrate_logs_to_internal(self, logs_queue: queue.Queue, queue_tag: str, kill_event: threading.Event) -> None:
         logger.info("Starting processing for queue {}".format(queue_tag))
 
         while not kill_event.is_set() or logs_queue.qsize() != 0:
@@ -517,7 +523,7 @@ class DatabaseManager:
                 elif queue_tag == "block":
                     self.pending_block_queue.put(x[-1])
 
-    def _update(self, table, columns, messages):
+    def _update(self, table: str, columns: List[str], messages: List[Dict[str, Any]]) -> None:
         try:
             self.db.update(table=table, columns=columns, messages=messages)
         except KeyboardInterrupt:
@@ -534,7 +540,7 @@ class DatabaseManager:
             except Exception:
                 logger.exception("Rollback failed")
 
-    def _insert(self, table, messages):
+    def _insert(self, table: str, messages: List[Dict[str, Any]]) -> None:
         try:
             self.db.insert(table=table, messages=messages)
         except KeyboardInterrupt:
@@ -551,8 +557,8 @@ class DatabaseManager:
             except Exception:
                 logger.exception("Rollback failed")
 
-    def _get_messages_in_batch(self, msg_queue):
-        messages = []  # type: List[Any]
+    def _get_messages_in_batch(self, msg_queue: "queue.Queue[X]") -> List[X]:
+        messages = []  # type: List[X]
         start = time.time()
         while True:
             if time.time() - start >= self.batching_interval or len(messages) >= self.batching_threshold:
@@ -567,7 +573,7 @@ class DatabaseManager:
                 messages.append(x)
         return messages
 
-    def close(self):
+    def close(self) -> None:
         logger.info("Database Manager cleanup initiated.")
         if not self.workflow_end and self.workflow_start_message:
             logger.info("Logging workflow end info to database due to abnormal exit")
@@ -579,19 +585,28 @@ class DatabaseManager:
                          columns=['run_id', 'time_completed',
                                   'workflow_duration'],
                          messages=[self.workflow_start_message])
-        self.batching_interval, self.batching_threshold = float(
-            'inf'), float('inf')
+        self.batching_interval = float('inf')
+        self.batching_threshold = float('inf')
         self._kill_event.set()
 
 
-def dbm_starter(exception_q, priority_msgs, node_msgs, block_msgs, resource_msgs, *args, **kwargs):
+def dbm_starter(exception_q: "queue.Queue[Tuple[str, str]]",
+                priority_msgs: "queue.Queue[Tuple[MessageType, Dict[str, Any]]]",
+                node_msgs: "queue.Queue[Dict[str, Any]]",
+                block_msgs: "queue.Queue[Dict[str, Any]]",
+                resource_msgs: "queue.Queue[Dict[str, Any]]",
+                db_url: str,
+                logdir: str,
+                logging_level: int) -> None:
     """Start the database manager process
 
     The DFK should start this function. The args, kwargs match that of the monitoring config
 
     """
     try:
-        dbm = DatabaseManager(*args, **kwargs)
+        dbm = DatabaseManager(db_url=db_url,
+                              logdir=logdir,
+                              logging_level=logging_level)
         logger.info("Starting dbm in dbm starter")
         dbm.start(priority_msgs, node_msgs, block_msgs, resource_msgs)
     except KeyboardInterrupt:

Makefile
@@ -49,7 +49,8 @@ mypy: ## run mypy checks
 	MYPYPATH=$(CWD)/mypy-stubs mypy parsl/tests/configs/
 	MYPYPATH=$(CWD)/mypy-stubs mypy parsl/tests/test*/
 	MYPYPATH=$(CWD)/mypy-stubs mypy parsl/tests/sites/
-	MYPYPATH=$(CWD)/mypy-stubs mypy parsl/app/ parsl/channels/ parsl/dataflow/ parsl/data_provider/ parsl/launchers parsl/providers/
+        # only the top level of monitoring is checked here because the visualization code does not type check
+	MYPYPATH=$(CWD)/mypy-stubs mypy parsl/app/ parsl/channels/ parsl/dataflow/ parsl/data_provider/ parsl/launchers parsl/providers/ parsl/monitoring/*py
 
 .PHONY: local_thread_test
 local_thread_test: ## run all tests with local_thread config

mypy.ini
@@ -12,8 +12,16 @@ warn_unused_ignores = True
 check_untyped_defs = True
 no_implicit_reexport = True
 disallow_subclassing_any = True
+warn_unreachable = True
+disallow_untyped_defs = True
 
 [mypy-parsl.tests.configs.local_user_opts]
+ignore_missing_imports = True
+
+[mypy-flask_sqlalchemy.*]
+ignore_missing_imports = True
+
+[mypy-networkx.*]
 ignore_missing_imports = True
 
 [mypy-sqlalchemy_utils.*]

parsl/monitoring/monitoring.py
@@ -12,7 +12,7 @@ from multiprocessing import Process, Queue
 from parsl.utils import RepresentationMixin
 
 from parsl.monitoring.message_type import MessageType
-from typing import Any, Dict, Optional
+from typing import Any, Callable, Dict, List, Optional, Union
 
 _db_manager_excepts: Optional[Exception]
 
@@ -28,7 +28,7 @@ else:
 logger = logging.getLogger(__name__)
 
 
-def start_file_logger(filename, name='monitoring', level=logging.DEBUG, format_string=None):
+def start_file_logger(filename: str, name: str = 'monitoring', level: int = logging.DEBUG, format_string: Optional[str] = None) -> logging.Logger:
     """Add a stream log handler.
 
     Parameters
@@ -63,15 +63,13 @@ def start_file_logger(filename, name='monitoring', level=logging.DEBUG, format_s
 
 class UDPRadio:
 
-    def __init__(self, monitoring_url, source_id=None, timeout=10):
+    def __init__(self, monitoring_url: str, source_id: int, timeout: int = 10):
         """
         Parameters
         ----------
 
         monitoring_url : str
             URL of the form <scheme>://<IP>:<PORT>
-        message : py obj
-            Python object to send, this will be pickled
         source_id : str
             String identifier of the source
         timeout : int
@@ -92,7 +90,7 @@ class UDPRadio:
                                   socket.IPPROTO_UDP)  # UDP
         self.sock.settimeout(self.sock_timeout)
 
-    def send(self, message):
+    def send(self, message: object) -> None:
         """ Sends a message to the UDP receiver
 
         Parameter
@@ -198,7 +196,7 @@ class MonitoringHub(RepresentationMixin):
         self.resource_monitoring_enabled = resource_monitoring_enabled
         self.resource_monitoring_interval = resource_monitoring_interval
 
-    def start(self, run_id):
+    def start(self, run_id: str) -> int:
 
         if self.logdir is None:
             self.logdir = "."
@@ -219,7 +217,7 @@ class MonitoringHub(RepresentationMixin):
                                                               min_port=self.client_port_range[0],
                                                               max_port=self.client_port_range[1])
 
-        comm_q = Queue(maxsize=10)  # type: Queue[Tuple[int, int]]
+        comm_q = Queue(maxsize=10)  # type: Queue[Union[Tuple[int, int], str]]
         self.exception_q = Queue(maxsize=10)  # type: Queue[Tuple[str, str]]
         self.priority_msgs = Queue()  # type: Queue[Tuple[Any, int]]
         self.resource_msgs = Queue()  # type: Queue[Tuple[Any, Any]]
@@ -269,7 +267,8 @@ class MonitoringHub(RepresentationMixin):
         self.monitoring_hub_url = "udp://{}:{}".format(self.hub_address, udp_dish_port)
         return ic_port
 
-    def send(self, mtype, message):
+    # TODO: tighten the Any message format
+    def send(self, mtype: MessageType, message: Any) -> None:
         self.logger.debug("Sending message {}, {}".format(mtype, message))
         try:
             self._dfk_channel.send_pyobj((mtype, message))
@@ -277,7 +276,7 @@ class MonitoringHub(RepresentationMixin):
             self.logger.exception(
                 "[MONITORING] The monitoring message sent from DFK to Hub timeouts after {}ms".format(self.dfk_channel_timeout))
 
-    def close(self):
+    def close(self) -> None:
         if self.logger:
             self.logger.info("Terminating Monitoring Hub")
         exception_msgs = []
@@ -304,17 +303,17 @@ class MonitoringHub(RepresentationMixin):
             self.logger.debug("Finished waiting for DBM termination")
 
     @staticmethod
-    def monitor_wrapper(f,
-                        try_id,
-                        task_id,
-                        monitoring_hub_url,
-                        run_id,
-                        logging_level,
-                        sleep_dur):
+    def monitor_wrapper(f: Any,
+                        try_id: int,
+                        task_id: int,
+                        monitoring_hub_url: str,
+                        run_id: str,
+                        logging_level: int,
+                        sleep_dur: float) -> Callable:
         """ Internal
         Wrap the Parsl app with a function that will call the monitor function and point it at the correct pid when the task begins.
         """
-        def wrapped(*args, **kwargs):
+        def wrapped(*args: List[Any], **kwargs: Dict[str, Any]) -> Any:
             # Send first message to monitoring router
             monitor(os.getpid(),
                     try_id,
@@ -349,18 +348,19 @@ class MonitoringHub(RepresentationMixin):
 class MonitoringRouter:
 
     def __init__(self,
-                 hub_address,
-                 hub_port=None,
-                 hub_port_range=(55050, 56000),
+                 *,
+                 hub_address: str,
+                 hub_port: Optional[int] = None,
+                 hub_port_range: Tuple[int, int] = (55050, 56000),
 
-                 client_address="127.0.0.1",
-                 client_port=None,
+                 client_address: str = "127.0.0.1",
+                 client_port: Optional[Tuple[int, int]] = None,
 
-                 monitoring_hub_address="127.0.0.1",
-                 logdir=".",
-                 run_id=None,
-                 logging_level=logging.INFO,
-                 atexit_timeout=3    # in seconds
+                 monitoring_hub_address: str = "127.0.0.1",
+                 logdir: str = ".",
+                 run_id: str,
+                 logging_level: int = logging.INFO,
+                 atexit_timeout: int = 3    # in seconds
                 ):
         """ Initializes a monitoring configuration class.
 
@@ -391,7 +391,6 @@ class MonitoringRouter:
                                         level=logging_level)
         self.logger.debug("Monitoring router starting")
 
-        self.hub_port = hub_port
         self.hub_address = hub_address
         self.atexit_timeout = atexit_timeout
         self.run_id = run_id
@@ -404,10 +403,11 @@ class MonitoringRouter:
                                   socket.IPPROTO_UDP)
 
         # We are trying to bind to all interfaces with 0.0.0.0
-        if not self.hub_port:
+        if not hub_port:
             self.sock.bind(('0.0.0.0', 0))
             self.hub_port = self.sock.getsockname()[1]
         else:
+            self.hub_port = hub_port
             self.sock.bind(('0.0.0.0', self.hub_port))
         self.sock.settimeout(self.loop_freq / 1000)
         self.logger.info("Initialized the UDP socket on 0.0.0.0:{}".format(self.hub_port))
@@ -428,7 +428,11 @@ class MonitoringRouter:
                                                            min_port=hub_port_range[0],
                                                            max_port=hub_port_range[1])
 
-    def start(self, priority_msgs, node_msgs, block_msgs, resource_msgs):
+    def start(self,
+              priority_msgs: "queue.Queue[Tuple[Tuple[MessageType, Dict[str, Any]], int]]",
+              node_msgs: "queue.Queue[Tuple[Dict[str, Any], int]]",
+              block_msgs: "queue.Queue[Tuple[Dict[str, Any], int]]",
+              resource_msgs: "queue.Queue[Tuple[Dict[str, Any], str]]") -> None:
 
         while True:
             try:
@@ -480,15 +484,39 @@ class MonitoringRouter:
         self.logger.info("Monitoring router finished")
 
 
-def router_starter(comm_q, exception_q, priority_msgs, node_msgs, block_msgs, resource_msgs, *args, **kwargs):
+def router_starter(comm_q: "queue.Queue[Union[Tuple[int, int], str]]",
+                   exception_q: "queue.Queue[Tuple[str, str]]",
+                   priority_msgs: "queue.Queue[Tuple[Tuple[MessageType, Dict[str, Any]], int]]",
+                   node_msgs: "queue.Queue[Tuple[Dict[str, Any], int]]",
+                   block_msgs: "queue.Queue[Tuple[Dict[str, Any], int]]",
+                   resource_msgs: "queue.Queue[Tuple[Dict[str, Any], str]]",
+
+                   hub_address: str,
+                   hub_port: Optional[int],
+                   hub_port_range: Tuple[int, int],
+
+                   client_address: str,
+                   client_port: Optional[Tuple[int, int]],
+
+                   logdir: str,
+                   logging_level: int,
+                   run_id: str) -> None:
 
     try:
-        router = MonitoringRouter(*args, **kwargs)
+        router = MonitoringRouter(hub_address=hub_address,
+                                  hub_port=hub_port,
+                                  hub_port_range=hub_port_range,
+                                  client_address=client_address,
+                                  client_port=client_port,
+                                  logdir=logdir,
+                                  logging_level=logging_level,
+                                  run_id=run_id)
     except Exception as e:
         logger.error("MonitoringRouter construction failed.", exc_info=True)
         comm_q.put(f"Monitoring router construction failed: {e}")
     else:
         comm_q.put((router.hub_port, router.ic_port))
+
     router.logger.info("Starting MonitoringRouter in router_starter")
     try:
         router.start(priority_msgs, node_msgs, block_msgs, resource_msgs)
@@ -499,14 +527,14 @@ def router_starter(comm_q, exception_q, priority_msgs, node_msgs, block_msgs, re
     router.logger.info("End of router_starter")
 
 
-def monitor(pid,
-            try_id,
-            task_id,
-            monitoring_hub_url,
-            run_id,
-            logging_level=logging.INFO,
-            sleep_dur=10,
-            first_message=False):
+def monitor(pid: int,
+            try_id: int,
+            task_id: int,
+            monitoring_hub_url: str,
+            run_id: str,
+            logging_level: int = logging.INFO,
+            sleep_dur: float = 10,
+            first_message: bool = False) -> None:
     """Internal
     Monitors the Parsl task's resources by pointing psutil to the task's pid and watching it and its children.
     """

commit 7382b9743798865010f3cf144113cb1011b2ab3b

router type annotations were incorrectly introduced in PR #1936 in 2021
they were introduced as `queue` module queues, but actually the values
(and the annotations used at the other end of the router_starter call)
are multiprocessing.Queue (subclasses...)

mypy does not typecheck this call... a later PR introduces typeguard here
(which detects this)

this hasn't caused execution problems - the APIs of the two queue types are close enough
- but it is incorrect when trying to understand how pieces of monitoring
communicate with each other.

parsl/monitoring/db_manager.py
@@ -1,5 +1,6 @@
 import datetime
 import logging
+import multiprocessing.queues as mpq
 import os
 import queue
 import threading
@@ -307,10 +308,10 @@ class DatabaseManager:
         self.pending_resource_queue = queue.Queue()  # type: queue.Queue[MonitoringMessage]
 
     def start(self,
-              priority_queue: "queue.Queue[TaggedMonitoringMessage]",
-              node_queue: "queue.Queue[MonitoringMessage]",
-              block_queue: "queue.Queue[MonitoringMessage]",
-              resource_queue: "queue.Queue[MonitoringMessage]") -> None:
+              priority_queue: "mpq.Queue[TaggedMonitoringMessage]",
+              node_queue: "mpq.Queue[MonitoringMessage]",
+              block_queue: "mpq.Queue[MonitoringMessage]",
+              resource_queue: "mpq.Queue[MonitoringMessage]") -> None:
 
         self._kill_event = threading.Event()
         self._priority_queue_pull_thread = threading.Thread(target=self._migrate_logs_to_internal,
@@ -722,11 +723,11 @@ class DatabaseManager:
 
 @wrap_with_logs(target="database_manager")
 @typeguard.typechecked
-def dbm_starter(exception_q: "queue.Queue[Tuple[str, str]]",
-                priority_msgs: "queue.Queue[TaggedMonitoringMessage]",
-                node_msgs: "queue.Queue[MonitoringMessage]",
-                block_msgs: "queue.Queue[MonitoringMessage]",
-                resource_msgs: "queue.Queue[MonitoringMessage]",
+def dbm_starter(exception_q: "mpq.Queue[Tuple[str, str]]",
+                priority_msgs: "mpq.Queue[TaggedMonitoringMessage]",
+                node_msgs: "mpq.Queue[MonitoringMessage]",
+                block_msgs: "mpq.Queue[MonitoringMessage]",
+                resource_msgs: "mpq.Queue[MonitoringMessage]",
                 db_url: str,
                 logdir: str,
                 logging_level: int) -> None:

parsl/monitoring/router.py
@@ -3,10 +3,10 @@ from __future__ import annotations
 import logging
 import os
 import pickle
-import queue
 import socket
 import threading
 import time
+from multiprocessing.queues import Queue
 from multiprocessing.synchronize import Event
 from typing import Optional, Tuple, Union
 
@@ -35,10 +35,10 @@ class MonitoringRouter:
                  run_id: str,
                  logging_level: int = logging.INFO,
                  atexit_timeout: int = 3,   # in seconds
-                 priority_msgs: "queue.Queue[AddressedMonitoringMessage]",
-                 node_msgs: "queue.Queue[AddressedMonitoringMessage]",
-                 block_msgs: "queue.Queue[AddressedMonitoringMessage]",
-                 resource_msgs: "queue.Queue[AddressedMonitoringMessage]",
+                 priority_msgs: "Queue[AddressedMonitoringMessage]",
+                 node_msgs: "Queue[AddressedMonitoringMessage]",
+                 block_msgs: "Queue[AddressedMonitoringMessage]",
+                 resource_msgs: "Queue[AddressedMonitoringMessage]",
                  exit_event: Event,
                  ):
         """ Initializes a monitoring configuration class.
@@ -211,12 +211,12 @@ class MonitoringRouter:
 
 @wrap_with_logs
 @typeguard.typechecked
-def router_starter(comm_q: "queue.Queue[Union[Tuple[int, int], str]]",
-                   exception_q: "queue.Queue[Tuple[str, str]]",
-                   priority_msgs: "queue.Queue[AddressedMonitoringMessage]",
-                   node_msgs: "queue.Queue[AddressedMonitoringMessage]",
-                   block_msgs: "queue.Queue[AddressedMonitoringMessage]",
-                   resource_msgs: "queue.Queue[AddressedMonitoringMessage]",
+def router_starter(comm_q: "Queue[Union[Tuple[int, int], str]]",
+                   exception_q: "Queue[Tuple[str, str]]",
+                   priority_msgs: "Queue[AddressedMonitoringMessage]",
+                   node_msgs: "Queue[AddressedMonitoringMessage]",
+                   block_msgs: "Queue[AddressedMonitoringMessage]",
+                   resource_msgs: "Queue[AddressedMonitoringMessage]",
                    exit_event: Event,
 
                    hub_address: str,